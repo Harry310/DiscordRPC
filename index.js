514d499aaa810185c8ae56f17e13455eda4c5534
Harry <h4rry_@outlook.com>
2021-06-22 19:48:12 +0100

    Add files

---
+++ index.js
@@ -0,0 +1,16 @@
+var rpc = require("discord-rpc")
+const client = new rpc.Client({ transport: 'ipc' })
+client.on('ready', () => {
+client.request('SET_ACTIVITY', {
+pid: process.pid,
+activity : {
+details : "Playing on PoliceRP",
+assets : {
+large_image : "largeimage",
+large_text : "Riverside Roleplay" // THIS WILL SHOW AS "Playing <Status>" from the outisde
+},
+buttons : [{label : "Connect" , url : "http://rsrp.uk/connect"},{label : "Website",url : "http://rsrp.uk"}]
+}
+})
+})
+client.login({ clientId : "856945568537116692" }).catch(console.error);
